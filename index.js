35fd2750e14c2cd57950022cd9108a87487d49e4
asonnenschein <adrian.sonnenschein@gmail.com>
2014-05-14 09:54:07 -0700

    restructure into node modules

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 #!/usr/bin/env node
 
 var async = require("async"),
-  expat = require("node-expat"),
-  parser = new expat.Parser("UTF-8"),
-  http = require("http"),
-  xmlStream = require("xml-stream");
+  parse = require("parse"),
+  archive = require("archive");
 
 var argv = require("yargs")
   .usage("Command line utility for archiving NGDS data on Amazon S3")
@@ -28,44 +26,15 @@ function constructRequest(startPosition, maxRecords) {
     elementSetName = "full";
   
   return {
-  	host: host,
-  	path: path + "Request=" + request + "&service=" + service + "&typeNames=" + 
-  	  typeNames + "&resultType=" + resultType + "&elementSetName=" + 
-  	  elementSetName + "&startPosition=" + startPosition + "&maxRecords=" +
-  	  maxRecords
+    host: host,
+    path: path + "Request=" + request + "&service=" + service + "&typeNames=" + 
+      typeNames + "&resultType=" + resultType + "&elementSetName=" + 
+      elementSetName + "&startPosition=" + startPosition + "&maxRecords=" +
+      maxRecords
   }
 }
 
-function scaleRequest (callback) {
-  var parameters = constructRequest(0, 0),
-    counter = 0,
-    total,
-    increment = 100;
-
-  http.get(parameters).on("response", function (response) {
-  	var xml = new xmlStream(response, "utf-8");
-  	xml.on("endElement: csw:SearchResults", function (results) {
-      total = results.$.numberOfRecordsMatched
-      while (counter < total) {
-  	    counter += increment;
-      }
-  
-      var placeHolder = (counter-increment),
-      lastRecord = ((increment-(counter-total))+placeHolder);
-
-      if (typeof callback === "function") {
-    	callback({
-  	      "increment": increment,
-  	      "placeHolder": placeHolder,
-  	      "lastRecord": lastRecord,
-        })
-      }
-  	})
-  }).end();
-}
-
 function parseCsw () {
-  scaleRequest(function (response) {
-  	console.log(response);
-  })
+  var parameters = constructRequest(0, 0);
+  
 }
---
+++ parse/index.js
@@ -0,0 +1,37 @@
+var parser = new expat.Parser("UTF-8"),
+  http = require("http"),
+  xmlStream = require("xml-stream");
+
+module.exports = {
+  scaleRequest: function (parameters, callback) {
+    var counter = 0,
+      total,
+      increment = 100;
+
+    http.get(parameters).on("response", function (response) {
+      var xml = new xmlStream(response, "utf-8");
+      xml.on("endElement: csw:SearchResults", function (results) {
+        total = results.$.numberOfRecordsMatched
+        while (counter < total) {
+          counter += increment;
+        }
+    
+        var placeHolder = (counter-increment),
+        lastRecord = ((increment-(counter-total))+placeHolder);
+
+        if (typeof callback === "function") {
+        callback({
+            "increment": increment,
+            "placeHolder": placeHolder,
+            "lastRecord": lastRecord,
+          })
+        }
+      })
+    }).end();
+  },
+  parseCsw: function () {
+    scaleRequest(function (response) {
+      console.log(response);
+    })
+  }
+};
