6dea62ff11fee58fb43a56afbae68310f2fb721b
asonnenschein <adrian.sonnenschein@gmail.com>
2014-06-03 10:00:32 -0700

    refactor error handlers with new flow-control

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var parse = require("./parse");
 var handle = require("./handle");
 var archiver = require("./archive");
 var utility = require("./utility");
+var timber = require("./timber");
 var path = require("path");
 var url = require("url");
 var fs = require("fs");
@@ -25,155 +26,15 @@ if (argv.archive) cmdQueue.push(doArchive);
 async.series(cmdQueue);
 
 function parseCsw () {
-  var datastore = new utility.datastore();
   var base = argv.out ? argv.out : path.dirname(require.main.filename);
   var dirs = utility.buildDirs(base);
   var vault = "ngds-archive";
-
-  function pinger (data, store, callback) {
-    async.forEach(data.linkages, function (linkage) {
-      var parsedUrl = url.parse(linkage);
-      var host = parsedUrl["protocol"] + "//" + parsedUrl["host"];
-      if (_.indexOf(store["unique"], host) === -1) {
-        store["unique"].push(host);
-        handle.pingUrl(host, function (error, response) {
-          if (error)
-            error["ping"] = "DEAD";
-            store["dead"].push(error);
-            store["status"].push(error);
-          if (response)
-            response["ping"] = "ALIVE";
-            store["status"].push(response);
-        })
-      }
-    })
-    callback(null, store);
-  };
-/*
-  function writeUrlStatus (store, logs, callback) {
-    async.series([
-      function (callback) {
-        handle.linkageLogger(store["unique"], logs["unique"], function (error, response) {
-          if (error) callback(null, error);
-          callback(null, response);
-        })
-      },
-      function (callback) {
-        handle.linkageLogger(store["status"], logs["status"], function (error, response) {
-          if (error) callback(null, error);
-          callback(null, response);
-        })
-      },
-      function (callback) {
-        handle.linkageLogger(store["dead"], logs["dead"], function (error, response) {
-          if (error) callback(null, error);
-          callback(null, response);
-        })
-      },
-    ],
-    function (error, results) {
-      if (error) callback(error);
-      callback(results);
-    })
-  }
-*/
-  function constructor (item, datastore, callback) {
-    var directory = path.join(dirs["record"], item.fileId);
-    var archived = path.join(dirs["archive"], item.fileId + ".zip");
-    var outXML = path.join(directory, item.fileId + ".xml");
-    var dead = _.map(datastore, function (record) {
-      if (record) return record["linkage"];
-    });
-
-    var construct = {
-      "directory": directory,
-      "archived": archived,
-      "outXML": outXML,
-      "fileId": item.fileId,
-      "linkages": item.linkages,
-      "fullRecord": item.fullRecord,
-      "deadLinks": dead,
-    }
-    callback(null, construct, datastore);
-  };
-
-  function processor (data, store, callback) {
-    var directory = data["directory"];
-    var archived = data["archived"];
-    var outXML = data["outXML"];
-    handle.buildDirectory(dirs, directory, function () {
-      handle.writeXML(outXML);
-      var counter = data.linkages.length;
-      var increment = 0;
-      async.each(data.linkages, function (linkage) {
-        var host = url.parse(linkage)["host"];
-        if (_.indexOf(data["dead"], host) === -1) {
-          if (linkage.search("service=WFS") !== -1) {
-            /*
-            parse.parseGetCapabilitiesWFS(linkage, function (linkages) {
-              async.forEach(linkages, function (linkage) {
-                handle.configurePaths(directory, linkage, function (res) {
-                  handle.buildDirectory(res.directory, function () {
-                    var urlQuery = url.parse(res.linkage)["query"];
-                    var typeName = querystring.parse(urlQuery)["typeNames"];
-                    if (typeName === "aasg:WellLog") {
-                      console.log("GOT A WELL LOG WFS");
-                      parse.parseWellLogsWFS(res.linkage, res.directory, function (response) {
-                        parse.parseWellLogs(response);
-                      })
-                    }
-
-                    if (typeName !== "aasg:WellLog") {
-                      parse.parseGetFeaturesWFS(res.linkage, res.directory, res.file, function (data) {
-                        
-                      })
-                    }                   
-                  })
-                })
-              })
-            })
-            */
-          } else {
-            handle.shouldDownload(linkage, function(err, res) {
-              if (res) {
-                handle.downloadFile(dirs, directory, res, function () {
-                  increment += 1;
-                  if (increment === counter) {
-                    callback(null, directory, archived);                
-                  }
-                });                
-              }
-            })
-          }
-        }
-      }) 
-    })
-  };
-
-  function zipper (uncompressed, compressed, callback) {
-    handle.compressDirectory(dirs, uncompressed, compressed, function () {
-      callback(null, uncompressed, compressed);
-    })
-  }
-
-  function vault (callback) {
-    archiver.checkGlacierVaults(vault, function (error, response) {
-      if (error) callback(error);
-      else callback(null);
-    });    
-  }
-
-  function iceberg (uncompressed, compressed, callback) {
-    archiver.uploadToGlacier(dirs, uncompressed, compressed, vault, function (error, response) {
-      if (error) callback(error);
-      else callback(response);
-    })
-  }
+  var dead = [];
+  var unique = [];
 
   var queue = async.queue(function (getRecordUrl, callback) {
-    
-    console.log(getRecordUrl);
-    
+    console.log(getRecordUrl["host"] + getRecordUrl["path"]);
+    var datastore = new utility.datastore();
     parse.parseCsw(getRecordUrl, function (data) {
       data.forEach(function (item) {
         async.waterfall([
@@ -185,6 +46,9 @@ function parseCsw () {
           function (callback) {
             pinger(item, datastore, callback);
           },
+          function (datastore, callback) {
+            logger(callback);
+          }
           function (datastore, callback) {
             constructor(item, datastore, callback);
           },
@@ -221,16 +85,128 @@ function parseCsw () {
       });
     })
   }
-
   startQueue();
+};
+
+function pinger (data, store, callback) {
+  async.forEach(data.linkages, function (linkage) {
+    var parsedUrl = url.parse(linkage);
+    var host = parsedUrl["protocol"] + "//" + parsedUrl["host"];
+    if (_.indexOf(store["unique"], host) === -1) {
+      store["unique"].push(host);
+      handle.pingUrl(host, function (error, response) {
+        if (error)
+          dead.push(host);
+          error["ping"] = "DEAD";
+          store["dead"].push(error);
+          store["status"].push(error);
+        if (response)
+          response["ping"] = "ALIVE";
+          store["status"].push(response);
+      })
+    }
+  })
+  callback(null, store);
+};
+
+function constructor (item, datastore, callback) {
+  var directory = path.join(dirs["record"], item.fileId);
+  var archived = path.join(dirs["archive"], item.fileId + ".zip");
+  var outXML = path.join(directory, item.fileId + ".xml");
+  var dead = _.map(datastore, function (record) {
+    if (record) return record["linkage"];
+  });
+
+  var construct = {
+    "directory": directory,
+    "archived": archived,
+    "outXML": outXML,
+    "fileId": item.fileId,
+    "linkages": item.linkages,
+    "fullRecord": item.fullRecord,
+    "deadLinks": dead,
+  }
+  callback(null, construct, datastore);
+};
+
+function processor (dead, store, callback) {
+  var directory = data["directory"];
+  var archived = data["archived"];
+  var outXML = data["outXML"];
+  handle.buildDirectory(dirs, directory, function () {
+    handle.writeXML(outXML);
+    var counter = data.linkages.length;
+    var increment = 0;
+    async.each(data.linkages, function (linkage) {
+      var host = url.parse(linkage)["host"];
+      if (_.indexOf(data["dead"], host) === -1) {
+        if (linkage.search("service=WFS") !== -1) {
+            /*
+            parse.parseGetCapabilitiesWFS(linkage, function (linkages) {
+              async.forEach(linkages, function (linkage) {
+                handle.configurePaths(directory, linkage, function (res) {
+                  handle.buildDirectory(res.directory, function () {
+                    var urlQuery = url.parse(res.linkage)["query"];
+                    var typeName = querystring.parse(urlQuery)["typeNames"];
+                    if (typeName === "aasg:WellLog") {
+                      console.log("GOT A WELL LOG WFS");
+                      parse.parseWellLogsWFS(res.linkage, res.directory, function (response) {
+                        parse.parseWellLogs(response);
+                      })
+                    }
+
+                    if (typeName !== "aasg:WellLog") {
+                      parse.parseGetFeaturesWFS(res.linkage, res.directory, res.file, function (data) {
+                        
+                      })
+                    }                   
+                  })
+                })
+              })
+            })
+            */
+        } else {
+          handle.shouldDownload(linkage, function(err, res) {
+            if (res) {
+              handle.downloadFile(dirs, directory, res, function () {
+                increment += 1;
+                if (increment === counter) {
+                  callback(null, directory, archived);                
+                }
+              });                
+            }
+          })
+        }
+      }
+    }) 
+  })
+};
+
+function zipper (uncompressed, compressed, callback) {
+  handle.compressDirectory(dirs, uncompressed, compressed, function () {
+    callback(null, uncompressed, compressed);
+  })
 }
 
+function vault (callback) {
+  archiver.checkGlacierVaults(vault, function (error, response) {
+    if (error) callback(error);
+    else callback(null);
+  });    
+}
 
+function iceberg (uncompressed, compressed, callback) {
+  archiver.uploadToGlacier(dirs, uncompressed, compressed, vault, function (error, response) {
+    if (error) callback(error);
+    else callback(response);
+  })
+}
 
+function logger (dirs, datastore, callback) {
+  timber.writeUrlStatus(dirs, datastore, function () {
 
-
-
-
+  })
+}
 
 
 
--- timber/index.js
+++ timber/index.js
@@ -14,10 +14,42 @@ module.exports = {
       "error_id": path.join(dirs["status"], "error_id.csv"),
     }
 
-//    fs.appendFile(logs[output], message, function (error) {
-//  		if (error) callback(error);
-//  		else callback();
-//  	})
-    callback();
+    var log = fs.createWriteStream(logs[output], {flags: "a"});
+    log.write(message);
+    log.on("close", function () {
+      callback(null, "Wrote log: " + logs[output]);
+    });
+    log.on("error", function (error) {
+      callback(error);
+    })
+  },
+  smartLog: function (store, callback) {
+    
+  },
+  writeUrlStatus: function (dirs, store, callback) {
+    var module = this;
+    async.series([
+      one: function (callback) {
+        module.log(dirs, "unique", store["unique"], function (err, res) {
+          if (err) callback(null, err);
+          else callback(null);
+        })
+      },
+      two: function (callback) {
+        module.log(dirs, "status", store["status"], function (err, res) {
+          if (err) callback(null, err);
+          else callback(null);
+        })
+      },
+      three: function (callback) {
+        module.log(dirs, "dead", store["dead"], function (err, res) {
+          if (err) callback(null, err);
+          else callback(null);
+        })
+      }
+    ], function (error, results) {
+      if (error) callback(error);
+      callback(results);
+    })
   }
 }
